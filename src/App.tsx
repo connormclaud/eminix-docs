da1899fe0e39cf5d8ed69b0fbbeebd0cf91d57b3
Vitali Vinahradski <connormclaud@gmail.com>
2026-04-07 04:56:24 +0200

    fix: use HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GapBufferPage from './pages/GapBufferPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/gap-buffer" element={<GapBufferPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
